0c396ffac4dad8d617d74245049d0ce9c5efe366
Anton <levashovwork@mail.ru>
2024-03-05 15:20:19 +0300

    не понимаю, до конца как поправить код так, чтобы ушла эта ошибка

--- DemoDogsApp/ViewController.swift
+++ DemoDogsApp/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     
     let tableView = UITableView()
+    var data = [DogsBreed]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         setupTableView()
-        fetchData()
+        fetchData(URL: "https://dog.ceo/api/breeds/list/all") { result in
+            self.data = result
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
         
             }
     
     func setupTableView() {
         view.addSubview(tableView)
+       
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
@@ -30,10 +39,9 @@ class ViewController: UIViewController {
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         
-        
     }
     
-    func fetchData() {
+    func fetchData(URL url: String, completion: @escaping([DogsBreed]) -> Void) {
         let urlString = "https://dog.ceo/api/breeds/list/all"
         
         guard let url = URL(string: urlString) else { return }
@@ -46,10 +54,9 @@ class ViewController: UIViewController {
             
             guard let data = data else { return }
             
-            
             do {
                 let bread = try JSONDecoder().decode([DogsBreed].self, from: data)
-                print(bread.first?.message)
+                completion(bread)
             } catch {
                 print(error)
             }
@@ -70,14 +77,16 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+      
         let cell = UITableViewCell()
         
-        cell.textLabel?.text = "hello"
+        cell.textLabel?.text = data[indexPath.row].message
         
         return cell
     }
